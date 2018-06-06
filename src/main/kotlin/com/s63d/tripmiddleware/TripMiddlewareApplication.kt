22a4d20686d17934eac1cdb9926a8e13f6bf0ddd
Marvin <marvin.zwolsman@gmail.com>
2018-06-06 16:59:21 +0200

    Initial commit

---
+++ src/main/kotlin/com/s63d/tripmiddleware/TripMiddlewareApplication.kt
@@ -0,0 +1,11 @@
+package com.s63d.tripmiddleware
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TripMiddlewareApplication
+
+fun main(args: Array<String>) {
+    runApplication<TripMiddlewareApplication>(*args)
+}
