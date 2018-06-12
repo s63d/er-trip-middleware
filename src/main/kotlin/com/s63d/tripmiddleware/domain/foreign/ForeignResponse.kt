1472532fdaec8e7d2d39bfe8d573697dda0886a4
Marvin <marvin.zwolsman@gmail.com>
2018-06-12 22:21:35 +0200

    Unknown as default country

--- src/main/kotlin/com/s63d/tripmiddleware/domain/foreign/ForeignResponse.kt
+++ src/main/kotlin/com/s63d/tripmiddleware/domain/foreign/ForeignResponse.kt
@@ -1,5 +1,5 @@
 package com.s63d.tripmiddleware.domain.foreign
 
 data class ForeignResponse(val id: String, val price: Double, val distance: Long, val vat:Int, val origin: String, val details: List<ForeignDetails>) {
-    private constructor() : this("", 0.0, 0,0, "", emptyList())
+    private constructor() : this("", 0.0, 0,0, "UNKNOWN", emptyList())
 }
