cec88b9a02369a4b53cf251319d73154c10d9818
Marvin <marvin.zwolsman@gmail.com>
2018-06-12 22:10:14 +0200

    Scheduled every 5sec to send hardcoded test request

--- src/main/kotlin/com/s63d/tripmiddleware/TripMiddlewareApplication.kt
+++ src/main/kotlin/com/s63d/tripmiddleware/TripMiddlewareApplication.kt
@@ -1,12 +1,12 @@
 package com.s63d.tripmiddleware
 
 import com.s63d.tripmiddleware.domain.Trip
-import com.s63d.tripmiddleware.domain.TripContainer
 import com.s63d.tripmiddleware.domain.Vehicle
 import com.s63d.tripmiddleware.producers.ForeignProducer
-import org.springframework.boot.CommandLineRunner
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.scheduling.annotation.EnableScheduling
+import org.springframework.scheduling.annotation.Scheduled
 import org.springframework.stereotype.Component
 
 @SpringBootApplication
@@ -18,12 +18,18 @@ fun main(args: Array<String>) {
 
 
 @Component
-class Tester(private val foreignProducer: ForeignProducer) : CommandLineRunner {
-    override fun run(vararg args: String?) {
+@EnableScheduling
+class Tester(private val foreignProducer: ForeignProducer) {
+    //var id:Long = 0xC0FFEE
 
-        //
-        //val trip = Trip(1111, "etjbHmq_iAac@sjD|fBwu@pJ`NhvAsVjSpBd{Bse@xYiv@fiBuf@bcB{v@`QyJrB`gBm@f`@inB|[{eAf}@cwAzYolBd_@{oAfjCa}@ja@wP}U??kUlE", Vehicle('a', 1000))
-        val trip = Trip(123, "}`eqHezrc@m|Ase@_Uw_CfRcwBdtAy}Cf]adCfiAytAfs@euCvAuhEd\\i{L`oDy~UvmKibWdrZmcWfsMehM`~O_e_@djPxwu@~{Dd}b@jrMlnMxpSytAdjU_{Kx~LmjZkUyy~@cfNmlrBr_\\_yhE|f|EskgU{sFcazAldFgm`A`zXsb^t{e@ikSzd[|yh@bxi@~}[rvWgr}Bqaa@m}mCoyYsxJ", Vehicle('a', 1000))
+    @Scheduled(fixedDelay = 5 * 1_000)
+    fun run() {
+        val trip = Trip(133, "}`eqHezrc@m|Ase@_Uw_CfRcwBdtAy}Cf]adCfiAytAfs@euCvAuhEd\\i{L`oDy~UvmKibWdrZmcWfsMehM`~O_e_@djPxwu@~{Dd}b@jrMlnMxpSytAdjU_{Kx~LmjZkUyy~@cfNmlrBr_\\_yhE|f|EskgU{sFcazAldFgm`A`zXsb^t{e@ikSzd[|yh@bxi@~}[rvWgr}Bqaa@m}mCoyYsxJ", Vehicle('a', 1000))
         foreignProducer.doRequest(trip)
+        //id += 1
+
+        //val trip = Trip(1111, "etjbHmq_iAac@sjD|fBwu@pJ`NhvAsVjSpBd{Bse@xYiv@fiBuf@bcB{v@`QyJrB`gBm@f`@inB|[{eAf}@cwAzYolBd_@{oAfjCa}@ja@wP}U??kUlE", Vehicle('a', 1000))
     }
 }
+
+
