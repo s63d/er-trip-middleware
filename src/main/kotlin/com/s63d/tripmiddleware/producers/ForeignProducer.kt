e73cc8d981b0b81dd872df975fb3a794b4538131
Marvin <marvin.zwolsman@gmail.com>
2018-06-12 12:13:58 +0200

    Producer uses constant in application.properties Correctly sending the foreign request to the correct queue name

--- src/main/kotlin/com/s63d/tripmiddleware/producers/ForeignProducer.kt
+++ src/main/kotlin/com/s63d/tripmiddleware/producers/ForeignProducer.kt
@@ -1,24 +1,42 @@
 package com.s63d.tripmiddleware.producers
 
 import com.s63d.tripmiddleware.domain.ForeignRequest
-import com.s63d.tripmiddleware.domain.ForeignVehicle
+import com.s63d.tripmiddleware.domain.ForeignRequestTripLocation
 import com.s63d.tripmiddleware.domain.Trip
 import com.s63d.tripmiddleware.utils.TripSplitter
+import org.slf4j.LoggerFactory
 import org.springframework.amqp.rabbit.core.RabbitTemplate
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Component
-import java.util.*
 
 @Component
 class ForeignProducer(private val rabbitTemplate: RabbitTemplate, private val tripSplitter: TripSplitter) {
 
-    fun doRequest(trip: Trip) {
-        val v = ForeignVehicle(trip.vehicle.rate,trip.vehicle.weight)
+    val logger = LoggerFactory.getLogger(this::class.java)
 
-        tripSplitter.splitTrip(trip).filter { it.country != "AT" }.forEach {
-            val req = ForeignRequest(UUID.randomUUID().toString(), v, it.polyline, "AT")
-            rabbitTemplate.convertAndSend("eu.req", it.country, req)
+    @Value("\${country}")
+    lateinit var COUNTRY_CODE: String
+
+    fun doRequest(completeTrip: Trip) {
+
+        val trips = tripSplitter.splitTrip(completeTrip)
+                .filter { it.country != COUNTRY_CODE }.groupBy { it.country }
+
+
+        for(trip in trips) {
+            val dest = trip.key
+            val req = ForeignRequest(completeTrip.id.toString(), completeTrip.vehicle.weight, trip.value.map {
+                it.locations.map {
+                    ForeignRequestTripLocation(Long.MIN_VALUE, it.y, it.x)
+                }
+            }, COUNTRY_CODE)
+
+
+            val key = "test_foreign_route_$dest"
+            logger.info("Sending request to $dest ($key)")
+            logger.info(req.toString())
+            rabbitTemplate.convertAndSend(key, req)
         }
-
     }
 
 }
