2311973e2d2f53a33b815bfc53f2ee226f0de7f3
Marvin <marvin.zwolsman@gmail.com>
2018-06-12 12:12:41 +0200

    Defined queues & direct exchange for internal usage

--- src/main/kotlin/com/s63d/tripmiddleware/config/RabbitConfig.kt
+++ src/main/kotlin/com/s63d/tripmiddleware/config/RabbitConfig.kt
@@ -1,5 +1,7 @@
 package com.s63d.tripmiddleware.config
 
+import org.springframework.amqp.core.DirectExchange
+import org.springframework.amqp.core.Queue
 import org.springframework.amqp.rabbit.connection.ConnectionFactory
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter
 import org.springframework.amqp.rabbit.core.RabbitTemplate
@@ -18,4 +20,13 @@ class RabbitConfig {
 
     @Bean
     fun jsonConverter() = Jackson2JsonMessageConverter()
+
+    @Bean
+    fun richRouteAT() = Queue("rich_route_AT", true)
+
+    @Bean
+    fun foreignRouteAT() = Queue("foreign_route_AT", true)
+
+    @Bean
+    fun internalExchange() = DirectExchange("AT")
 }
