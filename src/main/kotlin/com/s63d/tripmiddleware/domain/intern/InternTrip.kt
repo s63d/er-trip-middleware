1b4d0257ac448cf3b03bd12bfb967110b1f5277e
Marvin <marvin.zwolsman@gmail.com>
2018-06-16 09:18:09 +0200

    Default constructor

--- src/main/kotlin/com/s63d/tripmiddleware/domain/intern/InternTrip.kt
+++ src/main/kotlin/com/s63d/tripmiddleware/domain/intern/InternTrip.kt
@@ -1,3 +1,5 @@
 package com.s63d.tripmiddleware.domain.intern
 
-data class InternTrip (val id: Long, val polyline: String, val vehicleWeight: Int)
+data class InternTrip (val id: Long, val polyline: String, val vehicleWeight: Int) {
+    private constructor() : this(-1, "", -1)
+}
